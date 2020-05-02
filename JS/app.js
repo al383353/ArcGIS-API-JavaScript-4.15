9cfb87adcafd9761e0bb3c6ac9a938f4fe325181
senait <al383353@gmail.com>
2020-05-03 00:01:59 +0200

    going to vue

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,7 @@
 //https://developers.arcgis.com/javascript/latest/guide/vue/
 //https://developers.arcgis.com/javascript/latest/guide/webpack/
 //https://github.com/Esri/arcgis-js-cli
-//https://www.zdf.de/serien/rosamunde-pilcher/rosamunde-pilcher-zauber-der-liebe-100.html
-    require([
+//https://www.zdf.de/serien/rosamunde-pilcher/rosamunde-pilcher-wie-von-einem-anderen-stern-100.html    require([
         "esri/Map",
         "esri/views/MapView", //SceneView
         "esri/widgets/BasemapToggle",
